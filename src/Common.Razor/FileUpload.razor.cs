9b4d2d5ad7ef7cb5d339d360c94e92236ac2d8a8
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-08-31 21:22:56 +0100

    Added test project. Basic file uploader complete and working.

---
+++ src/Common.Razor/FileUpload.razor.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jpp.Common.Razor
+{
+    public class FileUploadBase : ComponentBase
+    {
+        public const int BufferSize = 4096;
+
+        [Parameter]
+        public byte[] File { get; set; }
+
+        [Inject]
+        public IJSRuntime Runtime { get; set; }
+
+        [Parameter]
+        public bool Uploading { get; set; } = false;
+
+        public bool NotUploading { get { return !Uploading; } }
+
+        public int ProgressPercentage { get; set; }
+
+        public bool UploadComplete { get; set; }
+
+        public FileUploadBase()
+        {            
+        }
+
+        public void UploadFile()
+        {
+            UploadComplete = false;
+            Task.Run(async () =>
+            {
+                //File = await _runtime.InvokeAsync<byte[]>("readFile", "");
+                int fileSize = await Runtime.InvokeAsync<int>("getFileSize", "fileUploadPicker");
+                File = new byte[fileSize];
+
+                int currentByte = 0;
+
+                Uploading = true;
+
+                while (Uploading)
+                {
+                    int toBeRead = BufferSize;
+
+                    if (toBeRead > fileSize - currentByte)
+                    {
+                        toBeRead = fileSize - currentByte;
+                        Uploading = false;
+                    }
+
+                    string buffer = await Runtime.InvokeAsync<string>("getSlice", "fileUploadPicker", currentByte, toBeRead);
+                    buffer = buffer.Replace("data:application/octet-stream;base64,", "");
+                    byte[] byteBuffer = Convert.FromBase64String(buffer);
+
+                    byteBuffer.CopyTo(File, currentByte);
+
+                    currentByte = currentByte + toBeRead;
+
+                    _ = InvokeAsync(() =>
+                      {
+                          double p = (double)currentByte / (double)fileSize * 100d;
+                          ProgressPercentage = (int)Math.Round(p);
+                          StateHasChanged();
+                      });
+                }                     
+
+                _ = InvokeAsync(() =>
+                {
+                    UploadComplete = true;
+                    StateHasChanged();
+                });
+            });            
+        }
+    }
+}
---
+++ src/Common.Razor/wwwroot/FileUploader.js
@@ -0,0 +1,23 @@
+﻿function getFileSize(fileInput) {
+    var file = document.getElementById(fileInput).files[0]; 
+    return file.size;    
+};
+
+async function getSlice(fileInput, start, end) {
+    return new Promise((resolve, reject) => {      
+    
+
+    var file = document.getElementById(fileInput).files[0];   
+    var blob = file.slice(start, start + end);
+
+    var reader = new FileReader();
+
+    reader.onloadend = function (e) {
+        resolve(e.target.result);
+    };
+
+     reader.readAsDataURL(blob);        
+
+    
+    });
+};
