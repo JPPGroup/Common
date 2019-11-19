50ad2d67a5d4298589e9d2f235784ccf2f5e22d4
jona <jon.abbott@jppuk.net>
2019-11-19 08:51:44 +0000

    disposable base classes

---
+++ src/Common/DisposableObject.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Jpp.Common
+{
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "Override DisposeManagedResources in derived classes, instead of Dispose")]
+    public abstract class DisposableManagedObject : IDisposable
+    {
+        public bool Disposed { get; private set; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!Disposed)
+            {
+                if (disposing)
+                {
+                    DisposeManagedResources();
+                }
+
+                Disposed = true;
+            }
+        }
+
+        protected virtual void DisposeManagedResources() { }
+    }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Design", "CA1063:Implement IDisposable Correctly", Justification = "Override DisposeManagedResources & DisposeUnmanagedResources in derived classes, instead of Dispose")]
+    public abstract class DisposableUnmanagedObject : IDisposable
+    {
+        public bool Disposed { get; private set; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~DisposableUnmanagedObject()
+        {
+            Debug.Assert(Disposed, "WARNING: Object finalized without being disposed!");
+            Dispose(false);
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!Disposed)
+            {
+                if (disposing)
+                {
+                    DisposeManagedResources();
+                }
+
+                DisposeUnmanagedResources();
+                Disposed = true;
+            }
+        }
+
+        protected virtual void DisposeManagedResources() { }
+        protected virtual void DisposeUnmanagedResources() { }
+    }
+}
