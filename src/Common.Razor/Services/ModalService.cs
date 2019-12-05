957e1542f40b2231309f0e697bb5e190e6bc988d
MichaelLiddiard <michael.liddiard@jppuk.net>
2019-12-05 23:57:10 +0000

    Sync

--- src/Common.Razor/Services/ModalService.cs
+++ src/Common.Razor/Services/ModalService.cs
@@ -11,14 +11,21 @@ namespace Jpp.Common.Razor.Services
         public event Action<string, RenderFragment> OnShow;
         public event Action OnClose;
 
-        public async void Show(string title, Type contentType)
+        public async void Show(string title, Type contentType, params KeyValuePair<string, string>[] attributes)
         {
             if (contentType.BaseType != typeof(ComponentBase))
             {
                 throw new ArgumentException($"{contentType.FullName} must be a Blazor Component");
             }
 
-            var content = new RenderFragment(x => { x.OpenComponent(1, contentType); x.CloseComponent(); });
+            //Pass parameters
+            var content = new RenderFragment(x => { 
+                x.OpenComponent(1, contentType);
+                foreach(KeyValuePair<string, string> attribute in attributes)
+                {
+                    x.AddAttribute(1, attribute.Key, attribute.Value);
+                }
+                x.CloseComponent(); });
             OnShow?.Invoke(title, content);
         }
 
