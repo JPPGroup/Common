050f773d3a2bc1163dd045fefb02ccdc1324b0b0
MichaelLiddiard <michael.liddiard@jppuk.net>
2020-02-18 08:52:04 +0000

    Created common download service for use by higher componenets

--- src/Common.Razor/wwwroot/Common.Razor.js
+++ src/Common.Razor/wwwroot/Common.Razor.js
@@ -30,3 +30,45 @@ async function getSlice(fileInput, start, end) {
 function clearInput(fileInput) {
     document.getElementById(fileInput).value = '';
 };
+
+/*function saveAsFile(filename, bytesBase64) {
+    if (navigator.msSaveBlob) {
+        //Download document in Edge browser
+        var data = window.atob(bytesBase64);
+        var bytes = new Uint8Array(data.length);
+        for (var i = 0; i < data.length; i++) {
+            bytes[i] = data.charCodeAt(i);
+        }
+        var blob = new Blob([bytes.buffer], { type: "application/octet-stream" });
+        navigator.msSaveBlob(blob, filename);
+    }
+    else {
+        var link = document.createElement('a');
+        link.download = filename;
+        link.href = "data:application/octet-stream;base64," + bytesBase64;
+        document.body.appendChild(link); // Needed for Firefox
+        link.click();
+        document.body.removeChild(link);
+    
+}*/
+
+function saveAsFile(filename) {
+    /*if (navigator.msSaveBlob) {
+        //Download document in Edge browser
+        var data = window.atob(bytesBase64);
+        var bytes = new Uint8Array(data.length);
+        for (var i = 0; i < data.length; i++) {
+            bytes[i] = data.charCodeAt(i);
+        }
+        var blob = new Blob([bytes.buffer], { type: "application/octet-stream" });
+        navigator.msSaveBlob(blob, filename);
+    }
+    else {*/
+        var link = document.createElement('a');
+    link.href = "api/download?name=" + filename;
+    link.download = filename;
+        document.body.appendChild(link); // Needed for Firefox
+        link.click();
+        document.body.removeChild(link);
+
+}
