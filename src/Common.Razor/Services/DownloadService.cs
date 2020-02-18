b435be1e62943029b6b98c79a1f5eee7a5c2b8bc
MichaelLiddiard <michael.liddiard@jppuk.net>
2020-02-18 08:52:04 +0000

    Created common download service for use by higher componenets

---
+++ src/Common.Razor/Services/DownloadService.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Jpp.Common.Razor.Services
+{
+    public class DownloadService
+    {
+        private Dictionary<string, MemoryStream> _registeredFiles;
+
+        public DownloadService()
+        {
+            _registeredFiles = new Dictionary<string, MemoryStream>();
+        }
+
+        public void Register(string filename, byte[] data)
+        {
+            byte[] header = Encoding.UTF8.GetBytes("base64,");
+            IEnumerable<byte> rv = header.Concat(data);
+
+            MemoryStream ms = new MemoryStream(rv.ToArray());
+            _registeredFiles[filename] = ms;
+        }
+
+        public MemoryStream Get(string filename)
+        {
+            MemoryStream result = _registeredFiles[filename];
+            _registeredFiles.Remove(filename);
+            return result;
+        }
+    }
+}
